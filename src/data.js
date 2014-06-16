5ce040a4e4ee91835a6461e76b277f9b87717207
Michal Budzynski <michal@virtualdesign.pl>
2014-06-16 20:13:13 +0200

    [JSNEWS-CLIENT] - Module separation

---
+++ src/data.js
@@ -0,0 +1,25 @@
+var config = require('./config');
+var FB = require('fb');
+
+module.exports = function(cb){
+  FB.api('oauth/access_token', {
+    client_id: config.client_id,
+    client_secret: config.client_secret,
+    grant_type: 'client_credentials'
+  }, function (res) {
+    if(!res || res.error) {
+      ccb(!res ? 'error occurred' : res.error);
+      return;
+    }
+
+    var accessToken = res.access_token;
+    FB.api('/' + config.groups[0] + '/feed?access_token='+accessToken, function (res) {
+      if(!res || res.error) {
+          cb(!res ? 'error occurred' : res.error);
+          return;
+      }
+      cb(null, res.data);
+    });
+
+  });
+}
