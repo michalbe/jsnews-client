cbd2bf16cc9484af33a8f46a17b4fbf241ba2f39
Michal Budzynski <michal@virtualdesign.pl>
2014-06-16 20:47:10 +0200

    [JSNEWS-CLIENT] - render author & part of the post

--- src/renderer.js
+++ src/renderer.js
@@ -9,17 +9,32 @@ var clearScreen = function(){
   console.log("\u001b[2J\u001b[0;0H");
 }
 
+var numberOfPosts = function() {
+  var posts = 'Ilość postów: '.magenta.bold + data.length.toString().yellow;
+  console.log(posts);
+}
+
+var post = function(nr, full) {
+  var currentPost = data[nr];
+  console.log('-------------------------'.magenta);
+  console.log('numer postu: '.magenta + nr.toString().magenta);
+  console.log('Autor: '.magenta.bold, currentPost.from.name.yellow);
+
+  var content = currentPost.message;
+  if (!full) {
+    content = content.substr(0, 150) + '...';
+  }
+  console.log('Treść: '.bold.magenta + content.yellow);
+  console.log(currentPost);
+}
+
 var all = function() {
   clearScreen();
   numberOfPosts();
-}
-
-var numberOfPosts = function() {
-  var posts = 'Ilość postów: ' + data.length;
-  console.log(posts.toString().magenta.bold);
+  post(4);
 }
 
 module.exports = {
   setData: setData,
-  all: all,
+  all: all
 }
