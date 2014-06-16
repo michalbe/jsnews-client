ff41b321ace008a3178a81c5d3e3e1f29a6bb002
Michal Budzynski <michal@virtualdesign.pl>
2014-06-16 20:25:30 +0200

    [JSNEWS-CLIENT] - Different approach on renderer

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var FBdata = require('./src/data');
+var renderer = require('./src/renderer');
 
 FBdata(function(err, data){
-  console.log(data);
+  renderer.setData(data);
+  renderer.all();
 });
