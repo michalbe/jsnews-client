fc42021ef7bd7a40b0b63fd29d7fa85a39202d7a
ChemikPIL <chemikpil@gmail.com>
2014-06-22 02:25:35 +0200

    Create colors theme

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,10 @@
 var colors = require('colors');
+var config = require('./config');
+
 var data;
 var postCount = 0;
+
+colors.setTheme(config.theme);
 
 var setData = function(d) {
   data = d;
@@ -12,7 +16,7 @@ var clearScreen = function(){
 }
 
 var numberOfPosts = function() {
-  var posts = 'Ilość postów: '.magenta.bold + postCount.toString().yellow;
+  var posts = 'Ilość postów: '.main.bold + postCount.toString().second;
   console.log(posts);
 }
 
@@ -23,58 +27,58 @@ var post = function(nr, full) {
   if (full) clearScreen();
 
   var currentPost = data[nr];
-  console.log('-------------------------'.magenta);
-  console.log('numer postu: '.magenta + nr.toString().magenta);
-  console.log('Autor: '.magenta.bold, currentPost.from.name.yellow);
+  console.log('-------------------------'.main);
+  console.log('numer postu: '.main + nr.toString().main);
+  console.log('Autor: '.main.bold, currentPost.from.name.second);
 
   var content = currentPost.message;
   if (!full) {
     content = content.substr(0, 150) + '...';
   }
-  console.log('Treść: '.bold.magenta + content.yellow);
+  console.log('Treść: '.main.bold + content.second);
 
   likes = currentPost.likes ? currentPost.likes.data.length : 0;
   comments = currentPost.comments ? currentPost.comments.data.length : 0;
   if (!full) {
     console.log(
-      'Lajki: '.bold.magenta +
-      likes.toString().yellow +
-      '  |  Komentarze: '.bold.magenta +
-      comments.toString().yellow
+      'Lajki: '.main.bold +
+      likes.toString().second +
+      '  |  Komentarze: '.main.bold +
+      comments.toString().second
     );
   } else {
     likes = likes > 0 ?
-      likes.toString().yellow + ' [ '.magenta +
+      likes.toString().second + ' [ '.main +
       currentPost.likes.data.map(
         function(l) {
           return l.name;
-        }).join(', ').yellow +
-      ' ]'.magenta
+        }).join(', ').second +
+      ' ]'.main
       : 0;
 
-    console.log('Lajki: '.bold.magenta + likes);
+    console.log('Lajki: '.main.bold + likes);
 
-    console.log('\nKomentarze:\n'.bold.magenta);
+    console.log('\nKomentarze:\n'.main.bold);
     if (comments > 0) {
       for (var i = 0; i < comments; i++) {
         comment(currentPost.comments.data[i]);
       }
     } else {
-      console.log('brak...'.italic.grey);
+      console.log('brak...'.disable.italic);
     }
   }
 
 }
 
 var comment = function(com) {
-  var date = new Date(com.created_time).toLocaleString().grey;
-  var author = com.from.name.magenta;
-  var msg = com.message.yellow;
-  var likes = com.like_count.toString().yellow;
+  var date = new Date(com.created_time).toLocaleString().disable;
+  var author = com.from.name.main;
+  var msg = com.message.second;
+  var likes = com.like_count.toString().second;
 
   console.log(date + '\n' + author);
   console.log(msg);
-  console.log('Lajki: '.magenta + likes + '\n');
+  console.log('Lajki: '.main + likes + '\n');
 }
 
 var all = function() {
