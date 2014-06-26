8b648ee8e91f2112d13b109266b736505c09a5c1
leriel <jedrzejg@jedrzejg-2.local>
2014-06-27 00:31:32 +0200

    Support for watch flags
    Done:
    FLAG_WATCH_NONE :sunglasses:
    FLAG_WATCH_NEW_POSTS
    FLAG_WATCH_NEW_POST_LIKES
    FLAG_WATCH_NEW_COMMENTS
    FLAG_WATCH_NEW_COMMENT_LIKES

--- index.js
+++ index.js
@@ -8,11 +8,18 @@ var config = require('./src/config');
 
 var currentGroup = -1;
 var currentPost = null;
-var currentCache = null;
-var lastCreatedPost = null;
+var groupCache = [];
+var groupLikeCache = [];
+var groupCommentCache = [];
+var postLikeCache = [];
+var postCommentCache = [];
+var postCommentLikeCache = [];
+var lastCreatedPosts = [];
+
 var FB = null;
 var stopRender = false;
 var follows = [];
+var notifyGroups = [];
 
 var renderGroupMenu = function () {
   var groups = [];
@@ -26,9 +33,7 @@ var renderGroupMenu = function () {
 
   renderer.clear();
   nav.showGroupList('Wybierz grupę', groups, function (answer) {
-    currentCache = null;
     currentGroup = config.groups[answer.option];
-    FB.setGroup(currentGroup.id);
     renderWall();
   });
 };
@@ -46,7 +51,7 @@ var renderWallMenu = function () {
 
 var wallActions = function (answer) {
   var postCount = renderer.getNumberOfPosts() - 1;
-  
+
   answer = answer.option.toLowerCase();
 
   switch(answer) {
@@ -93,7 +98,7 @@ var removeFollowPost = function (answer) {
   if (option.indexOf('all') > -1) {
     follows = [];
   } else if (option.length && option.indexOf('back') === -1) {
-    for (var i = option.length-1; i > -1; i--) {    
+    for (var i = option.length-1; i > -1; i--) {
       follows.splice(parseInt(option[i], 10),1);
     }
   }
@@ -164,14 +169,14 @@ var postActions = function (answer) {
   }
 };
 
-var checkLatestPost = function (post) {
-  if (post.created_time === post.updated_time && lastCreatedPost !== post.id) {
+var checkLatestPost = function (group, post) {
+  if (post.created_time === post.updated_time && lastCreatedPosts[group.id] !== post.id) {
     notification(
-      currentGroup.name,
+      group.name,
       post.from.name + ' dodał(a) nowy post',
       post.message && post.message.substr(0, 50) + '...'
     );
-    lastCreatedPost = post.id;
+    lastCreatedPosts[group.id] = post.id;
   }
 };
 
@@ -189,22 +194,155 @@ var checkFollowPosts = function (post, id) {
     }
 
     follows[id] = data;
-  }); 
+  });
+};
+
+var checkLikes = function (group, data) {
+  var postLikeData = {};
+  data.forEach(function (post) {
+    if(post.likes && post.likes.data)
+    {
+      postLikeData[post.id] = {
+        likes: post.likes.data,
+        message: post.message.length > 50 ? post.message.substr(0, 50) + '...' : post.message
+      };
+    }
+  });
+
+  var likeDataString = JSON.stringify(postLikeData);
+
+  if (groupLikeCache && groupLikeCache[group.id] && groupLikeCache[group.id] === likeDataString) {
+    return;
+  }
+
+  for (var index in postLikeData) {
+    var likeData = postLikeData[index];
+    if(!postLikeCache[index] || JSON.stringify(likeData) !== JSON.stringify(postLikeCache[index]))
+    {
+      var cacheLength = postLikeCache[index] && postLikeCache[index].likes ? postLikeCache[index].likes.length : 0;
+      var countNewLikes = likeData.likes.length - cacheLength;
+      var strOthers = countNewLikes > 1 ? ' i ' + (countNewLikes - 1) + ' innych' : '';
+      notification(
+        group.name,
+        likeData.likes[0].name + strOthers + ' lubi post',
+        likeData.message
+      );
+
+    }
+    postLikeCache[index] = likeData;
+  }
+  groupLikeCache[group.id] = likeDataString;
+};
+
+var checkGroupCommentsAndLikes = function (group, data) {
+  var postCommentData = {},
+    commentsLikeData = {};
+  data.forEach(function (post) {
+    if(post.comments && post.comments.data)
+    {
+      postCommentData[post.id] = {
+        comments: post.comments.data,
+        message: post.message.length > 50 ? post.message.substr(0, 50) + '...' : post.message
+      };
+      if(group.watchFlags & config.watchFlags.FLAG_WATCH_NEW_COMMENT_LIKES) {
+        post.comments.data.forEach(function (comment) {
+          commentsLikeData[comment.id] = {
+            likes: comment.like_count,
+            message: comment.message.length > 50 ? comment.message.substr(0, 50) + '...' : comment.message
+          };
+        });
+      }
+    }
+  });
+
+  var commentDataString = JSON.stringify(postCommentData);
+  var commentLikeDataString = JSON.stringify(commentsLikeData);
+
+  if (groupCommentCache && groupCommentCache[group.id] && groupCommentCache[group.id] === commentDataString) {
+    return;
+  }
+
+  for (var index in postCommentData) {
+    var commentData = postCommentData[index];
+    if(!postCommentCache[index] || JSON.stringify(commentData) !== JSON.stringify(postCommentCache[index]))
+    {
+      var cacheLength = postCommentCache[index] && postCommentCache[index].comments ? postCommentCache[index].comments.length : 0;
+      var countNewComments = commentData.comments.length - cacheLength;
+      if(countNewComments > 0 && group.watchFlags & config.watchFlags.FLAG_WATCH_NEW_COMMENTS)
+      {
+        var strOthers = countNewComments > 1 ? ' i ' + (countNewComments - 1) + ' innych' : '';
+        var lastCommentIndex = commentData.comments.length - 1;
+        var message = commentData.comments[lastCommentIndex].message.length > 50 ? commentData.comments[lastCommentIndex].message.substr(0, 50) + '...' : commentData.comments[lastCommentIndex].message;
+        var title = commentData.comments[lastCommentIndex].from.name + strOthers + ' skomentował post: ' + commentData.message;
+        notification(
+          group.name,
+          title,
+          message
+        );
+      }
+      else if (countNewComments <= 0 && group.watchFlags & config.watchFlags.FLAG_WATCH_NEW_COMMENT_LIKES &&
+        postCommentLikeCache && postCommentLikeCache[group.id] && JSON.stringify(postCommentLikeCache[group.id]) !== commentLikeDataString) {
+        for (var commentId in commentsLikeData) {
+          var commentLikeData = commentsLikeData[commentId];
+          if(commentLikeData.likes > postCommentLikeCache[group.id][commentId].likes) {
+            notification(
+              group.name,
+              'Nowe polubienie komentarza',
+              commentLikeData.message
+            );
+          }
+        }
+      }
+    }
+    postCommentCache[index] = commentData;
+  }
+  groupCommentCache[group.id] = commentDataString;
+  postCommentLikeCache[group.id] = commentsLikeData;
+
+};
+
+var checkForNotifications = function () {
+
+  notifyGroups.forEach(function(group, index) {
+    try {
+      FB.getWall(group.id, function (err, data) {
+        if(index === notifyGroups.length-1) {
+          setTimeout(checkForNotifications, config.refreshTime);
+        }
+
+        var dataString = JSON.stringify(data);
+
+        if (groupCache && groupCache[group.id] && groupCache[group.id] === dataString) {
+          return;
+        }
+
+        groupCache[group.id] = JSON.stringify(data);
+        if(group.watchFlags & config.watchFlags.FLAG_WATCH_NEW_POSTS) {
+          checkLatestPost(group, data[0]);
+        }
+        if(group.watchFlags & config.watchFlags.FLAG_WATCH_NEW_POST_LIKES) {
+          checkLikes(group, data);
+        }
+        if(group.watchFlags & config.watchFlags.FLAG_WATCH_NEW_COMMENTS || group.watchFlags & config.watchFlags.FLAG_WATCH_NEW_COMMENT_LIKES) {
+          checkGroupCommentsAndLikes(group, data);
+        }
+        for (var i = 0, l = follows.length; i < l; i++) {
+          checkFollowPosts(follows[i], i);
+        }
+      });
+
+    }
+    catch (e) {
+      console.log(e);
+    }
+  });
+
+
 };
 
 var renderWall = function () {
-  FB.getWall(function (err, data) {
+  FB.getWall(currentGroup.id, function (err, data) {
     setTimeout(renderWall, config.refreshTime);
-
-    var dataString = JSON.stringify(data);
-
-    if (currentCache && currentCache === dataString) return;
-
-    currentCache = JSON.stringify(data);
-    checkLatestPost(data[0]);
-    for (var i = 0, l = follows.length; i < l; i++) {
-      checkFollowPosts(follows[i], i);   
-    }
 
     renderer.setData(data);
 
@@ -229,6 +367,16 @@ var init = function () {
     data(function (err, api) {
       FB = api;
       renderGroupMenu();
+      config.groups.forEach(function(group, index) {
+        if(group.watchFlags !== undefined && group.watchFlags !== config.watchFlags.FLAG_WATCH_NONE)
+        {
+          notifyGroups.push(group);
+        }
+      });
+      if(notifyGroups.length > 0)
+      {
+        checkForNotifications();
+      }
     });
   }
 };
